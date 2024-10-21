719209223467d6e167d78a08b51a3b4400fec8d2
Leandro Cardoso <leandro.cardoso@spread.com.br>
2024-10-21 07:45:53 -0300

    Adicionando script de MFA

---
+++ Script_Powershell/Cadastra_MFA_Massa.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+
+connect-graph
+$users = Import-Csv -Path "caminho" -Delimiter ";" -Encoding Default
+
+foreach ($user in $users){
+if ($user.ColunaComTelefone -ne $null){
+Write-Host "Não há telefone para cadastrado de MFA"
+}else{
+
+New-MgUserAuthenticationPhoneMethod -UserId "inserir UPN" -PhoneType "mobile" -PhoneNumber ("55" + $user.ColunaComTelefone)
+
+}
+}
